f12dbd43b4a9631bcc92a346e48ef87e3b0fd4ef
filipegbessa <filipegbessa@gmail.com>
2023-08-02 20:49:51 -0300

    feat(): create DissaffectionEntity and OffenseEntity

--- src/main/java/com/filipeabessa/SysGuardeiNoCorazonBackend/disaffection/DisaffectionEntity.java
+++ src/main/java/com/filipeabessa/SysGuardeiNoCorazonBackend/disaffection/DisaffectionEntity.java
@@ -1,4 +1,80 @@
 package com.filipeabessa.SysGuardeiNoCorazonBackend.disaffection;
 
+import com.filipeabessa.SysGuardeiNoCorazonBackend.offense.OffenseEntity;
+
+import java.util.Date;
+import java.util.List;
+
 public class DisaffectionEntity {
+    private long id;
+    private String title;
+    private String description;
+    private Date createdAt;
+    private Date updatedAt;
+    private String witnesses;
+    private String involvedPeople;
+    private List<OffenseEntity> offenses;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getWitnesses() {
+        return witnesses;
+    }
+
+    public void setWitnesses(String witnesses) {
+        this.witnesses = witnesses;
+    }
+
+    public List<OffenseEntity> getOffenses() {
+        return offenses;
+    }
+
+    public void setOffenses(List<OffenseEntity> offenses) {
+        this.offenses = offenses;
+    }
+    public String getInvolvedPeople() {
+        return involvedPeople;
+    }
+
+    public void setInvolvedPeople(String involvedPeople) {
+        this.involvedPeople = involvedPeople;
+    }
 }
--- src/main/java/com/filipeabessa/SysGuardeiNoCorazonBackend/offense/OffenseEntity.java
+++ src/main/java/com/filipeabessa/SysGuardeiNoCorazonBackend/offense/OffenseEntity.java
@@ -1,4 +1,59 @@
 package com.filipeabessa.SysGuardeiNoCorazonBackend.offense;
 
 public class OffenseEntity {
+    private long id;
+    private long disaffectionId;
+
+    private String title;
+    private String description;
+    private String cursedFamilyMember;
+    private String offendingPerson;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getDisaffectionId() {
+        return disaffectionId;
+    }
+
+    public void setDisaffectionId(long disaffectionId) {
+        this.disaffectionId = disaffectionId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCursedFamilyMember() {
+        return cursedFamilyMember;
+    }
+
+    public void setCursedFamilyMember(String cursedFamilyMember) {
+        this.cursedFamilyMember = cursedFamilyMember;
+    }
+
+    public String getOffendingPerson() {
+        return offendingPerson;
+    }
+
+    public void setOffendingPerson(String offendingPerson) {
+        this.offendingPerson = offendingPerson;
+    }
 }
